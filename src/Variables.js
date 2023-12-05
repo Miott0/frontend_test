d8a84de217a38ccc0ca4bea4b158a369a1739a9f
Miott0 <miotterafael@gmail.com>
2023-12-05 10:19:25 -0300

    feat

--- src/Variables.js
+++ src/Variables.js
@@ -8,7 +8,7 @@ export const variables = {
     variables.PHOTO_URL = "http://127.0.0.1:8000/Photos/";
   } else {
     // Se você quiser fornecer valores padrão para o ambiente de produção, substitua as strings vazias abaixo.
-    variables.API_URL = "";
-    variables.PHOTO_URL = "";
+    variables.API_URL = "https://backend-test-rouge.vercel.app/";
+    variables.PHOTO_URL = "https://backend-test-rouge.vercel.app/Photos/";
   }
   
