0ceee15a655e584f2a6be22e02602827691325b9
Miott0 <miotterafael@gmail.com>
2023-12-05 09:37:32 -0300

    'feat'

--- src/Variables.js
+++ src/Variables.js
@@ -1,9 +1,14 @@
-export const variables={
-    API_URL:"http://127.0.0.1:8000/",
-    PHOTO_URL:"http://127.0.0.1:8000/Photos/"
-
-
-    process.env.NODE_ENV === "development"
-        ? ""
-        : "";
-}
+export const variables = {
+    API_URL: "",
+    PHOTO_URL: "",
+  };
+  
+  if (process.env.NODE_ENV === "development") {
+    variables.API_URL = "http://127.0.0.1:8000/";
+    variables.PHOTO_URL = "http://127.0.0.1:8000/Photos/";
+  } else {
+    // Se você quiser fornecer valores padrão para o ambiente de produção, substitua as strings vazias abaixo.
+    variables.API_URL = "";
+    variables.PHOTO_URL = "";
+  }
+  
